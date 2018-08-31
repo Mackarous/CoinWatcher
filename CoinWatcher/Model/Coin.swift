dd6fd90e1a1fc0280971dc0537f02712afc8d1b2
Andrew Mackarous <a.mackarous@gmail.com>
2018-08-31 16:26:12 -0400

    added decodable FetchedCoin object

---
+++ CoinWatcher/Model/Coin.swift
@@ -0,0 +1,35 @@
+//
+//  Model.swift
+//  CoinWatcher
+//
+//  Created by Andrew Mackarous on 2018-08-31.
+//  Copyright © 2018 Brave. All rights reserved.
+//
+
+import Foundation
+
+struct FetchedCoin: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case price = "price_cad"
+    }
+    let id: String
+    let name: String
+    let price: Decimal
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        let priceString = try container.decode(String.self, forKey: .price)
+        price = Decimal(string: priceString) ?? 0
+    }
+}
+
+extension Decimal {
+    var currencyString: String? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        return numberFormatter.string(from: self as NSDecimalNumber)
+    }
+}
